3f1b8b1026e7c7ba5896d17c7a2eeb6b33447ba2
Christian5262 <chrismai1020162016@hotmail.com>
2023-07-04 18:48:10 -0300

    Add endpoint post

--- Api/src/handlers/usersHandlers.js
+++ Api/src/handlers/usersHandlers.js
@@ -1,25 +1,35 @@
 // Controllers:
 
+const createClient = require("../../Controller/createClient");
+
 // 
 
 // Handlers:
 
-const getUsers = (req,res) => {
-  return res.status(200).json({ DIY: "En esta ruta se obtendran todos los usuarios (clientes/proveedores)"})
+const getUsers = (req, res) => {
+  return res.status(200).json({ DIY: "En esta ruta se obtendran todos los usuarios (clientes/proveedores)" })
 };
 
-const getUser = (req,res) => {
-  const {id} = req.params;
-  return res.status(200).json({DIY: `En esta ruta se obtendrá el usuario de id: ${id}`});
-};
-
-const postUser = (req,res) => {
-  return res.status(200).json({DIY: "En esta ruta se creará el usuario"});
-};
-
-const putUser = (req,res) => {
+const getUser = (req, res) => {
   const { id } = req.params;
-  return res.status(200).json({DIY: `En esta ruta se actualizará el usuario de id: ${id}`});
+  return res.status(200).json({ DIY: `En esta ruta se obtendrá el usuario de id: ${id}` });
+};
+
+const postUser = async (req, res) => {
+  const { name, email, image, genre, rating, description, active, pro } = req.body
+  try {
+    const client = { name, email, image, genre, rating, description, active, pro }
+    const infoClient = await createClient(client)
+    return res.status(200).json(infoClient)
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+
+};
+
+const putUser = (req, res) => {
+  const { id } = req.params;
+  return res.status(200).json({ DIY: `En esta ruta se actualizará el usuario de id: ${id}` });
 };
 
 module.exports = {
