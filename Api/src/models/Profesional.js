28de4421482dfe287227be157fe7e8fdc3977578
NathalyQuiva <nathalyquiva2@gmail.com>
2023-07-05 14:21:50 -0500

    Endpoints: name, profesionals, borrado logico

--- Api/src/models/Profesional.js
+++ Api/src/models/Profesional.js
@@ -1,62 +1,6 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-<<<<<<< HEAD
- const Profesional= sequelize.define('Profesional', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING(25),
-      allowNull: false,
-      unique: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    genre: {
-      type: DataTypes.STRING(25),
-      allowNull: false,
-    },
-    rating: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-      defaultValue: null,
-    },
-    years_exp: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-    pro: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-    softDelete: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true,
-      defaultValue:null,
-    }
-
-  }, {
-=======
   sequelize.define("Profesional",
   {
    id:{
@@ -105,8 +49,12 @@ module.exports = (sequelize) => {
     allowNull: false,
     defaultValue: true,
    },
+   softDelete: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+    defaultValue:null,
+  }
   },{
->>>>>>> develop
     timestamps: false,
   });
 };
