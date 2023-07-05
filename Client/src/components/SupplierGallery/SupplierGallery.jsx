ab6532426ee64b81bec4063bfba5064e8d941814
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-07-05 09:44:00 -0600

    Creacion del modulo JSX para SupplierGallery.

---
+++ Client/src/components/SupplierGallery/SupplierGallery.jsx
@@ -0,0 +1,10 @@
+export default function SupplierGallery () {
+  return (
+    <div>
+      <h3>Galeria de Imagenes</h3>
+      <div className="container">
+        <div className="gallery"></div>
+      </div>
+    </div>
+  )
+}
