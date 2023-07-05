e4a3dd98ca3fbc9a14ce1cdfac802a490d1a8ebb
CristianCuesta14 <110426875+CristianCuesta14@users.noreply.github.com>
2023-07-05 00:25:50 -0500

    consiguracion de redux & creacion componente navbar

--- Client/NavBar/Navbar.jsx
+++ Client/NavBar/Navbar.jsx
@@ -1,8 +1,56 @@
+//import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Navbar = () => {
-    return (
+  // Estado para controlar la visibilidad del desplegable
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Función para alternar la visibilidad del desplegable
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Función para manejar el clic en una opción
+  const handleOptionClick = (option) => {
+    console.log("Selected option:", option);
+    setIsOpen(false); // Cerrar el desplegable al seleccionar una opción
+  };
+  return (
+    <div>
+      <nav>
         <div>
-            Este es el componente NavBar
+          <Link to="/home">aqui viene el logo</Link>
+          <Link to="/comofunciona">Cómo funciona</Link>
         </div>
-    )
-}
-export default Navbar
+        <div>
+          <ul>
+            <li>
+              <a href="#" onClick={toggleDropdown}>
+                Registrate ▼
+              </a>
+              {isOpen && (
+                <ul>
+                  <li>
+                    <a href="#" onClick={() => handleOptionClick("cliente")}>
+                      Soy Cliente
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={() => handleOptionClick("profesional")}
+                    >
+                      Soy Profesional
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import "./App.css";
+import Navbar from "../NavBar/Navbar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <Navbar/>
+     
       <h2>Hola Mundo</h2>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
