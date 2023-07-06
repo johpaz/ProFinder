adb7bd823be6e8c29d448be4117d7d66c9ae7991
Maria Jose Olaechea <olaecheamariajose@gmail.com>
2023-07-06 01:28:26 -0300

    arreglo de errores

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,17 @@
+import { FormProvider } from "react-hook-form";
 import "./App.css";
 import Navbar from "./components/navBar/Navbar";
-import HowDoesItWork from "./components/HowDoesItWork/HowDoesItWork";
+
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage/HomePage";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/comofunciona" element={<HowDoesItWork />} />
-        <h2>Hola Mundo</h2>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<FormProvider />} />
       </Routes>
     </div>
   );
