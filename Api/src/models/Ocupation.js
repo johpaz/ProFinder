0754156d3f4369da9023ee644eb19af2f42d7b97
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-07 01:23:25 -0300

    Model ocupation validation

--- Api/src/models/Ocupation.js
+++ Api/src/models/Ocupation.js
@@ -11,6 +11,11 @@ module.exports = (sequelize) => {
     name:{
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate:{
+        is: /^[a-zA-Z\s]+$/, // Nombre debe ser una palabra o frase sin números ni símbolos
+        len: [3,15]
+      }
     }
   },{
     timestamps: false,
