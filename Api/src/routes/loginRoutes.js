54d677c30bb4b6ea333c6aba68768c1ce497956d
NathalyQuiva <nathalyquiva2@gmail.com>
2023-07-07 05:16:27 -0500

    Hasheo de password completado

--- Api/src/routes/loginRoutes.js
+++ Api/src/routes/loginRoutes.js
@@ -2,19 +2,18 @@ const { Router } = require('express');
 
 const { sequelize } = require("../db");
 
-const { registerUser }=require('../handlers/registerHandler');
+const { loginUser }=require('../handlers/loginHandler');
+
+
+
 
 const loginRouter = Router();
 
-loginRouter.post('/', async (req,res)=>{
-    const {email,password}= req.body;
 
-        const sql= await sequelize.query(`SELECT * FROM "Users" where email= '${email}' AND password= '${password}'`)
+loginRouter.post('/', loginUser);
 
-        if(!sql[0].length){
-         res.status(401).json({message: "Incorrect credentials"})}
-        else {
-        res.status(200).json({message: "Correct credentials"})
-    }});
 
-module.exports = loginRouter;
+
+
+
+module.exports= loginRouter;
--- Api/src/routes/registerRoutes.js
+++ Api/src/routes/registerRoutes.js
@@ -1,26 +1,16 @@
 const { Router } = require('express');
 
-const {sequelize} = require("../db");
+const { userRegister}=require('../handlers/registerHandler');
 
-const bcrypt= require("bcrypt");
 
-const { registerUser }=require('../handlers/registerHandler');
+
 
 const registerRouter = Router();
 
-registerRouter.post('/', async (req,res)=>{
-    const {name,email,password}= req.body;
-    //bcrypt.hash(password, 8, (error, hash)=>{
-      //  if (error) throw
-    //})//entre 8 y 10 saltos para que sea segura pero no tome mucho tiempo
-    try {
-        const sql= await sequelize.query(`INSERT INTO "Users" (name,email,password) VALUES ('${name}','${email}','${password}')`)
+registerRouter.post('/', userRegister)
 
-        if(sql.length!==0){
-        return res.status(201).json({message: "User created successfully"})}
-    } catch (error) {
-        res.status(400).json({error: error.message})
-        
-    }});
+
+
+
 
 module.exports = registerRouter;
