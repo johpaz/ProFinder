2b46945d0e7b41664a04e76cb127cfe5d8d8b4d7
John <johpaz252@gmail.com>
2023-07-09 16:24:16 -0500

    post client ok

--- Api/src/controllers/postsClientControllers/index.js
+++ Api/src/controllers/postsClientControllers/index.js
@@ -1,4 +1,7 @@
-const getAllPostsByClients = require("./01 - getAllPostsByClients");
+const {getAllPostsByClients,getAllPostsByClientsApi} = require("./01 - getAllPostsByClients");
 const createPost = require("./02 - createPost")
 
-module.exports = { getAllPostsByClients, createPost };
+module.exports = { getAllPostsByClients,
+    getAllPostsByClientsApi,
+    createPost
+ };
