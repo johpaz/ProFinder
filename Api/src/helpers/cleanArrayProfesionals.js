f2821248bd7a96c9871525bae4753acc7ca42831
John <johpaz252@gmail.com>
2023-07-10 08:20:50 -0500

    rating

--- Api/src/helpers/cleanArrayProfesionals.js
+++ Api/src/helpers/cleanArrayProfesionals.js
@@ -18,6 +18,7 @@ const cleanArray = (profesionals) => {
       name: profesional.name,
       email: profesional.email,
       image: profesional.image,
+      rating: profesional.rating,
       genre: profesional.genre,
       years_exp: profesional.years_exp,
       phone: profesional.phone,
