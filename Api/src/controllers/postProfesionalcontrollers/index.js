a51e01500b467e074a8a68aacf02fe4273c71783
John <johpaz252@gmail.com>
2023-07-11 09:57:06 -0500

    solucion

--- Api/src/controllers/postProfesionalcontrollers/index.js
+++
@@ -1,4 +0,0 @@
-const createPostProfesional = require("./01 - createPostProfesional");
-const {getAllPostsByProfesionals,getAllPostsByProfesionalsApi} = require("./02 - getPostsProfesionals");
-
-module.exports = { createPostProfesional, getAllPostsByProfesionals,getAllPostsByProfesionalsApi }
