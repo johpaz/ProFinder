ac8de67d8cbfdd04c1b21afae5568135f5350f4a
John Paez <120274363+johpaz@users.noreply.github.com>
2023-07-11 10:12:08 -0500

    Revert "solucion"

--- Api/src/controllers/ocupationControllers/07 - getMessage.js
+++
@@ -1,8 +0,0 @@
-
-
-const getMessage= ()=>{
-    
-    return ("Debe ingresar una ocupacion para buscar a los profesionales por ocupacion")
-}
-
-module.exports=getMessage;
--- Api/src/controllers/postsClientControllers/index.js
+++
@@ -1,7 +0,0 @@
-const {getAllPostsByClients,getAllPostsByClientsApi, getPostsClient} = require("./01 - getAllPostsByClients");
-const createPost = require("./02 - createPost")
-
-module.exports = { getAllPostsByClients,
-    getAllPostsByClientsApi,
-    createPost,getPostsClient
- };
