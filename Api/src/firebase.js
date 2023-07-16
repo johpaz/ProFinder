d639ec2554c6340d552125e59d863132d9ddd44a
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-16 08:03:58 -0300

    Iniciando firebase-admin y tengo la url que llega en image

---
+++ Api/src/firebase.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+const storage = admin.storage();
+
+const getImageUrl = async (filePath) => {
+  try {
+    const bucket = storage.bucket();
+    const file = bucket.file(filePath);
+    const [url] = await file.getSignedUrl({
+      action: 'read',
+      expires: '03-01-2500',
+    });
+    console.log("Yey")
+    return url;
+  } catch (error) {
+    console.log(`Error al obtener la url de la imagen ${error}`);
+    throw error
+  }
+};
+
+module.exports = { getImageUrl };
