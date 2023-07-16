c7bab4bfaed329c283dab85f1115b369a29dbd75
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-16 19:35:06 -0300

    get Category opt

--- Api/src/controllers/categoryControllers/index.js
+++ Api/src/controllers/categoryControllers/index.js
@@ -1,4 +1,4 @@
-const { getAllCategories, getCategoriesByName,getAllCategoriesApi } = require('./01 - getCategories');
+const { getAllCategories, getCategoriesByName,getAllCategoriesApi,getCategoriesBdd } = require('./01 - getCategories');
 const getCategoryById = require('./02 - getCategoriesById');
 const createCategory = require('./03 - createCategory');
 const createCateProf = require('./04 - createCateProf');
@@ -6,6 +6,6 @@ const createCateProf = require('./04 - createCateProf');
 
 module.exports = {
   getAllCategories,getCategoriesByName,
-  getCategoryById,getAllCategoriesApi,
+  getCategoryById,getAllCategoriesApi,getCategoriesBdd,
   createCategory,createCateProf
 }// 4ef29225941cb9bb0ea93f9cae9b3bcb614f46f8
