477fa3c0bdf32868caf98b8bff780e1f1abd3adb
NathalyQuiva <nathalyquiva2@gmail.com>
2023-07-20 06:47:55 -0500

    commit

--- Api/src/middlewares/google/google.js
+++ Api/src/middlewares/google/google.js
@@ -1,15 +1,9 @@
-
-const passport= require ('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const dotenv = require('dotenv').config()
 
 const {sequelize} = require("../../db");
 
-
-var  use =require ('passport');
 var {config}= require('dotenv');
-const { all } = require('../../routes');
-// const { response } = require('../..');
+
 config();
 
 
@@ -19,94 +13,37 @@ config();
 const allEmails= async()=>{
   const allemails= await sequelize.query('SELECT email FROM "Users" ')
 
-console.log(allemails[0])
-
-
-
-
   const emailsdb=[];
+  
   for (var i = 0; i < allemails[0].length; i++){
-            const newEmail= allemails[0][i].email
-            //console.log(newEmail)
-           emailsdb.push(newEmail);
+    const newEmail= allemails[0][i].email;
+    emailsdb.push(newEmail);
+  } return (emailsdb)
+};
+
+
+const execute= async (accessToken, refreshToken, profile, done) =>{
+  const emails= await allEmails();
+
+  const response= emails.includes(profile.emails[0].value);
   
-        
-        }return(emailsdb)
+  if (response){console.log('si hay res')
   
+    done(null, profile)
+  
+  } else { console.log('nohay res')
+    done(null, profile)
+  }
+};
    
-  
-    }
-
-  
-
-;
 
 
-//console.log(allemails[0])
-
-
-
-
-  
-
-
-  // const emails= (arrayEmails(allEmails))
-  // console.log(emails)
-
- 
-    //  console.info(emailsdb)
-
-
-
-
-
-// // const emails= async ()=>{
-//   const allEmails=  async ()=>{ await sequelize.query('SELECT email FROM "Users" ')}
-// console.log(allEmails)
-// // return  allEmails
-// // };
-
-
-const emails=["nathalyquiva@gmail.com", "jdmontanez3@gmail.com"];//base de datos que tienen los gmails
-
-
-
-
-const authgoogle =new GoogleStrategy({
+const authgoogle =new GoogleStrategy ({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3001/auth/google"//primera autentica y por ahora lleva a home
-  },
-  function(accessToken, refreshToken, profile, done) {
-  //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-  //      console.log("llegue aca")
-  //     return cb(err, user);
+  }, execute)
 
-
-
-  //const allEmails=  sequelize.query('SELECT email FROM "Users" ');
-
-const response= emails.includes(profile.emails[0].value);
-console.log('estoy aca ')
-console.log(allEmails())
-//console.log(allemails())
-console.log(response)
-if (response){
-  console.log(response)
-  const isEmailBd= emails.includes(profile.emails[0].value)
-console.log(isEmailBd)
-  console.log('si hay res')
-  done(null, isEmailBd)//profile)
-
-} else {
-  console.log('nohay res')
-  // emails.push(profile.emails[0].value);
-  done(null, profile)
-}
-}
-    )
-
-;
 
 module.exports= authgoogle
 
