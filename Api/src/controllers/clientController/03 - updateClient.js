855c1975eaea9e5195e68f986440ca69fa716996
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-21 12:41:31 -0300

    put profesional

--- Api/src/controllers/clientController/03 - updateClient.js
+++ Api/src/controllers/clientController/03 - updateClient.js
@@ -5,6 +5,7 @@ const { Op } = require('sequelize');
 const { getImageUrl } = require('../../firebase');
 
 const updateClient = async (id, name, email, password, image, genre, description, phone, CountryId, LocationId) => {
+    if(!Number(id)) throw Error(`Compruebe los datos para registrarse`);
  
     const clientInBDD = await Client.findByPk(id);
     // console.log(clientInBDD);
@@ -34,6 +35,35 @@ const updateClient = async (id, name, email, password, image, genre, description
     if (image) {
         imageUrl = image[0]
     };
+
+    if(CountryId && LocationId){
+        clientInBDD.name = name || clientInBDD.name;
+        clientInBDD.email = email || clientInBDD.email;
+        clientInBDD.password = password || clientInBDD.password;
+        clientInBDD.image = imageUrl || clientInBDD.image;
+        clientInBDD.genre = genre || clientInBDD.genre;
+        clientInBDD.description = description || clientInBDD.description;
+        clientInBDD.phone = phone || clientInBDD.phone;
+        clientInBDD.CountryId = CountryId || clientInBDD.CountryId;
+        clientInBDD.LocationId = LocationId || clientInBDD.LocationId;
+        await clientInBDD.save();
+        const countryBDD = await Country.findByPk(CountryId);
+        const locationBDD = await Location.findByPk(LocationId);
+    
+        await clientInBDD.setCountry(countryBDD.id);
+        await clientInBDD.setLocation(locationBDD.id);
+        return {
+            id: clientInBDD.id,
+            name: clientInBDD.name,
+            email: clientInBDD.email,
+            image: clientInBDD.image,
+            genre: clientInBDD.genre,
+            description: clientInBDD.description,
+            phone: clientInBDD.phone,
+            country: countryBDD.name,
+            location: locationBDD.name,
+        };
+    }
 
     clientInBDD.name = name || clientInBDD.name;
     clientInBDD.email = email || clientInBDD.email;
