c4f0a96641ffceed3eb9cf869f88572b8c0b8c72
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-08-06 12:58:56 -0300

    cleanArrayTop

---
+++ Api/src/helpers/cleanArrayTopProfesionals.js
@@ -0,0 +1,38 @@
+const cleanArrayTopProfesionals = (profesionals) => {
+  return profesionals.map((profesional)=>{
+    const professions = profesional.Categories.map((category) => {
+      const ocupations = profesional.Ocupations.filter(
+        (ocupation) => ocupation.CategoryId === category.id
+      );
+      return {
+        id: category.id,
+        category: category.name,
+        ocupations: ocupations.map((ocupation) => ({
+          id: ocupation.id,
+          name: ocupation.name,
+        })),
+      };
+    });
+  
+    return {
+      id: profesional.id,
+      name: profesional.name,
+      email: profesional.email.toLowerCase(),
+      softDelete: profesional.softDelete,
+      active: profesional.active,
+      image: profesional.image,
+      rating: profesional.rating,
+      genre: profesional.genre,
+      years_exp: profesional.years_exp,
+      phone: profesional.phone,
+      ubication: {
+        country: profesional.Country ? profesional.Country.dataValues.name : null,
+        location: profesional.Location ? profesional.Location.dataValues.name : null,
+      },
+      geolocation:profesional.geolocation,
+      professions: professions,
+    };
+  })
+};
+
+module.exports = cleanArrayTopProfesionals
